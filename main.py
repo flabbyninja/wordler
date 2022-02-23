21b8994d593067bd6c6225f9c7d0cfe50262d118
Flabby Ninja <dev@flabbyninja.net>
2022-02-23 02:34:13 +0000

    Merge patterns between floating and locked, returning words that match any of those

--- main.py
+++ main.py
@@ -16,21 +16,22 @@ def get_words_specified_length(length, input_data):
     return list(map(lambda x: x.lower(), filter(lambda x: len(x) == length, input_data)))
 
 
-def get_words_confirmed_position(finalized_letters, word):
+def get_words_locked_position(finalized_letters, word):
     for i, x in enumerate(word):
-        if (finalized_letters[i] is not None) and (finalized_letters[i] != x):
+        if (finalized_letters[i] != '_') and (finalized_letters[i] != x):
             return False
     return True
 
 
-def get_words_loose_pattern(pattern_list, word_list):
+def get_words_from_pattern(pattern_list, word_list):
     potential_words = []
     for pattern in pattern_list:
-        potential_words += list(filter(lambda x: is_match(pattern, x), word_list))
+        potential_words += list(
+            filter(lambda x: is_word_a_pattern_match(pattern, x), word_list))
     return potential_words
 
 
-def is_match(pattern, word):
+def is_word_a_pattern_match(pattern, word):
     if len(pattern) != len(word):
         return False
     for i, c in enumerate(pattern):
@@ -39,20 +40,36 @@ def is_match(pattern, word):
     return True
 
 
-def generate_letter_permutations(letters):
+def generate_letter_permutations(letters, word_length):
     # Get all permutations of [1, 2, 3]
+    if len(letters) < word_length:
+        letters += '_' * (word_length - len(letters))
+    if len(letters) > word_length:
+        letters = letters[:word_length]
     perm = permutations(letters)
-    pattern_output = []
+    permutation_output = []
 
     # Print the obtained permutations
-    for i in list(perm):
-        pattern_output.append("".join([str(elem) for elem in i]))
+    for i in set(perm):
+        permutation_output.append("".join([str(elem) for elem in i]))
 
-    return pattern_output
+    return permutation_output
 
 
-def initialise_words():
-    return
+def merge_patterns(locked_letters, permutation):
+    merged_permutations = set()
+    for perm in permutation:
+        accept = True
+        for i, c in enumerate(perm):
+            if locked_letters[i] != '_':
+                if c != '_':
+                    accept = False
+                    break
+                else:
+                    perm = perm[:i] + locked_letters[i] + perm[i+1:]
+        if accept:
+            merged_permutations.add(perm)
+    return merged_permutations
 
 
 if __name__ == '__main__':
@@ -60,20 +77,14 @@ if __name__ == '__main__':
     english_words = load_words(test_data)
     five_words = get_words_specified_length(5, english_words)
 
-    test_data = [None for x in range(5)]
-    test_data[1] = 'l'
-    test_data[4] = 'e'
+    floating_letters = 'o'
+    locked_letters = 't__r_'
+    word_length = 5
 
-    # filtered_words = list(
-    #     filter(lambda x: get_words_confirmed(test_data, x), five_words))
+    possible_permutations = generate_letter_permutations(
+        floating_letters, word_length)
 
-    # print(is_match('a_p_e', 'apple'))
-    # print(is_match('r_al', 'apple'))
-    # print(is_match('a__l_lll', 'apple'))
-    # print(is_match('_p_l_', 'apple'))
-    # print(is_match('apple', 'apple'))
-    # print(is_match('_____', 'apple'))
-    # print(filtered_words)
+    # merge permutations and reduce by known letter positions
+    valid_permutations = merge_patterns(locked_letters, possible_permutations)
 
-    # print(generate_letter_permutations(['a', '_', 'p', '_', 'e']))
-    print(get_words_loose_pattern(['a_p_e'], five_words))
+    print(get_words_from_pattern(valid_permutations, five_words))
