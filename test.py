0bfb91d3d8f915a351b30d436c30bbb801d1789d
Flabby Ninja <dev@flabbyninja.net>
2022-02-25 00:54:23 +0000

    Add ability to remove known letters in specific positions, and cater for duplicate letters Fixes #7

--- test.py
+++ test.py
@@ -1,4 +1,3 @@
-from cgi import test
 import unittest
 import mapper
 
@@ -12,6 +11,8 @@ class TestGetWordsSpecifiedLength(unittest.TestCase):
     def test_non_zero_exists(self):
         filtered_words = mapper.get_words_specified_length(5, self.test_words)
         self.assertEqual(filtered_words, ['Fiver', 'Alone', 'Alive', 'Irate'])
+        filtered_words = mapper.get_words_specified_length(3, self.test_words)
+        self.assertEqual(filtered_words, ['Sun'])
 
     def test_non_zero_notexist(self):
         filtered_words = mapper.get_words_specified_length(10, self.test_words)
@@ -126,30 +127,46 @@ class TestGenerateLetterPermutations(unittest.TestCase):
 class TestMergePatterns(unittest.TestCase):
     def setUp(self):
         self.test_permutations = set(
-            ['_ct', '_tc', 'c_t', 't_c', 'ct_', 'tc_'])
+            ['_ct_', '_tc_', 'c_t_', 't_c_', 'ct__', 'tc__'])
 
-    def test_merge_valid(self):
+    def test_merge_all_valid(self):
         self.assertSetEqual(mapper.merge_patterns(
-            '_a_', self.test_permutations), set(['tac', 'cat']))
-        self.assertSetEqual(mapper.merge_patterns(
-            'a__', self.test_permutations), set(['act', 'atc']))
+            '___a', set(['c___', '_t__']), self.test_permutations), set(['_cta', 't_ca', 'tc_a']))
 
     def test_merge_inequal_length(self):
-        self.assertSetEqual(mapper.merge_patterns(
-            '___a', self.test_permutations), self.test_permutations)
-        self.assertSetEqual(mapper.merge_patterns(
-            'a_', self.test_permutations), set(['act', 'atc']))
+        self.assertSetEqual(
+            mapper.merge_patterns('____a', set(
+                ['c_', '_t_____']), self.test_permutations), set(['_ct_', 't_c_', 'tc__'])
+        )
+        self.assertSetEqual(
+            mapper.merge_patterns(
+                'a_', set(['c_', '_t_____']), self.test_permutations), set(['act_']))
 
     def test_merge_empty_permutations(self):
-        self.assertSetEqual(mapper.merge_patterns('_a_', set()), set(['_a_']))
+        self.assertSetEqual(mapper.merge_patterns(
+            '_a_', set(), set()), set(['_a_']))
+
+    def test_merge_empty_floating(self):
+        self.assertSetEqual(mapper.merge_patterns(
+            '_a_', set(), self.test_permutations), set(['cat_', 'tac_']))
+
+    def test_merge_empty_locked_floating(self):
+        self.assertSetEqual(mapper.merge_patterns(
+            '', set(), self.test_permutations), self.test_permutations)
+
+    def test_merge_floating_locked_overlap(self):
+        self.assertSetEqual(mapper.merge_patterns(
+            'ca__', set(['___c', '_t__']), self.test_permutations), set(['cat_']))
+
+    def test_merge_empty_locked(self):
+        self.assertSetEqual(mapper.merge_patterns('', set(
+            ['c__', '_t__']), self.test_permutations), set(['_ct_', 't_c_', 'tc__']))
+        self.assertSetEqual(mapper.merge_patterns(None, set(
+            ['c__', '_t__']), self.test_permutations), set(['_ct_', 't_c_', 'tc__']))
 
     def test_merge_all_locked_letters(self):
         self.assertSetEqual(mapper.merge_patterns(
-            'cat', self.test_permutations), set(['cat']))
-
-    def test_merge_no_locked_letters(self):
-        self.assertSetEqual(mapper.merge_patterns(
-            '___', self.test_permutations), self.test_permutations)
+            'cate', set(), self.test_permutations), set(['cate']))
 
 
 class TestCalcLetterFrequency(unittest.TestCase):
@@ -201,34 +218,74 @@ class TestCollectFloatingLetters(unittest.TestCase):
         self.floating_patterns = ['i____', '____s', '_r_m_']
 
     def test_collect_standard(self):
-        self.assertSetEqual(mapper.collect_floating_letters(self.floating_patterns, 5),
-                            set(['i', 's', 'r', 'm']))
+        self.assertListEqual(sorted(mapper.collect_floating_letters(self.floating_patterns, 5)),
+                             ['i', 'm', 'r', 's'])
 
     def test_collect_multi_same_letter(self):
         multi_patterns = self.floating_patterns.copy()
-        multi_patterns.append('__i__')
-        self.assertSetEqual(mapper.collect_floating_letters(multi_patterns, 5),
-                            set(['i', 's', 'r', 'm']))
+        multi_patterns.append('__i_i')
+        self.assertListEqual(sorted(mapper.collect_floating_letters(multi_patterns, 5)),
+                             ['i', 'i', 'm', 'r', 's'])
 
     def test_collect_patterns_blank(self):
-        self.assertSetEqual(
-            mapper.collect_floating_letters(['_____'], 5), set())
-        self.assertSetEqual(mapper.collect_floating_letters([], 5), set())
+        self.assertListEqual(
+            mapper.collect_floating_letters(['_____'], 5), [])
+        self.assertListEqual(mapper.collect_floating_letters([], 5), [])
 
     def test_collect_patterns_none(self):
-        self.assertIsNone(mapper.collect_floating_letters(None, 5), set())
+        self.assertIsNone(mapper.collect_floating_letters(None, 5), [])
 
     def test_collect_pattern_size_smaller_than_template(self):
-        self.assertSetEqual(mapper.collect_floating_letters(
-            ['_a_b_c'], 5), set(['a', 'b']))
+        self.assertListEqual(sorted(mapper.collect_floating_letters(
+            ['_a_b_c'], 5)), ['a', 'b'])
 
     def test_collect_zero_pattern_size(self):
-        self.assertSetEqual(mapper.collect_floating_letters(
-            ['_a_b_c'], 0), set([]))
+        self.assertListEqual(mapper.collect_floating_letters(
+            ['_a_b_c'], 0), [])
 
     def test_collect_pattern_size_larger_than_template(self):
-        self.assertSetEqual(mapper.collect_floating_letters(
-            ['_a_b_c'], 10), set(['a', 'b', 'c']))
+        self.assertListEqual(sorted(mapper.collect_floating_letters(
+            ['_a_b_c'], 10)), ['a', 'b', 'c'])
+
+
+class TestProcessAllPatterns(unittest.TestCase):
+    def setUp(self):
+        self.input_patterns = ['a__b_', '_aa_c', 'b__bz', 'z__c_']
+
+    def test_process_standard(self):
+        self.assertListEqual(
+            mapper.process_all_patterns(self.input_patterns),
+            [{'a': 1, 'b': 1}, {'a': 2, 'c': 1}, {'b': 2, 'z': 1}, {'z': 1, 'c': 1}])
+
+    def test_process_empty(self):
+        self.assertListEqual(
+            mapper.process_all_patterns([]), [])
+
+    def test_process_all_blank(self):
+        self.assertListEqual(
+            mapper.process_all_patterns(['_____', '_____', '_____']),  [])
+
+    def test_process_none(self):
+        self.assertIsNone(mapper.process_all_patterns(None))
+
+
+class TestReducePatterns(unittest.TestCase):
+    def setUp(self):
+        self.input_patterns = [{'a': 1, 'b': 2},
+                               {'c': 2, 'b': 1}, {'a': 4, 'z': 1}]
+
+    def test_reduce_standard(self):
+        self.assertDictEqual(mapper.reduce_patterns(self.input_patterns), {
+                             'a': 4, 'b': 2, 'c': 2, 'z': 1})
+
+    def test_reduce_none(self):
+        self.assertIsNone(mapper.reduce_patterns(None))
+
+    def test_reduce_empty(self):
+        self.assertDictEqual(mapper.reduce_patterns([{}, {}, {}]),
+                             {})
+        self.assertDictEqual(mapper.reduce_patterns([]),
+                             {})
 
 
 if __name__ == '__main__':
