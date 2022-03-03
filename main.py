52832edf8f62a7eaa06d700e92c956dc03e504ed
Flabby Ninja <dev@flabbyninja.net>
2022-03-03 15:41:29 +0000

    Clean up main processing config

--- main.py
+++ main.py
@@ -1,37 +1,31 @@
 import wordlertools.pattern_processor as pattern_processor
 
+from typing import Set
 
-def perform_processing():
 
-    candidate_words = pattern_processor.get_candidate_words(
+def perform_processing() -> Set[str]:
+
+    # Init and output behaviour
+    words_file = './data/words_alpha.txt'
+    word_length = 5
+
+    return pattern_processor.get_candidate_words(
         locked_pattern, floating_patterns, excluded_letters, words_file, word_length)
-
-    if show_possible_words:
-        print(candidate_words)
-
-    # top_letters = mapper.calc_letter_frequency(
-    #     candidate_words, permutation_letters, locked_letters)
-
-    # if show_top_letter:
-    #     top_letter, _ = top_letters.most_common(1)[0]
-    #     print(top_letter)
 
 
 if __name__ == '__main__':
 
     # ADD CONFIG HERE #
+    show_possible_words = True
 
     # Word specific
     locked_pattern = '_a_t_'
     floating_patterns = {'__n_s'}
     excluded_letters = 'erip'
 
-    # Init and output behaviour
-    words_file = './data/words_alpha.txt'
-    word_length = 5
-    show_possible_words = True
-    show_top_letter = False
-
     # END CONFIG #
 
-    perform_processing()
+    candidate_words = perform_processing()
+
+    if show_possible_words:
+        print(candidate_words)
