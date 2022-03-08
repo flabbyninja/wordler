8d0784e710f64286ecc70ba7baf2222dc87a7505
Flabby Ninja <dev@flabbyninja.net>
2022-03-08 20:27:42 +0000

    Update dependencies - Wrap and expose ready for Lambda deployment Fixes #12

--- build.sh
+++ build.sh
@@ -3,17 +3,20 @@
 # mkdir -p build && cp wordler.py build
 # mkdir -p build/wordlertools && cp -R wordlertools/* build/wordlertools
 
+export BUILD_DIR="build"
 export PKG_DIR="python"
 
 rm -rf ${PKG_DIR} &&\
     rm package.zip &&\
     mkdir -p ${PKG_DIR}
 
-docker run --rm -v $(pwd):/foo -w /foo lambci/lambda:build-python3.8 \
+cd ${BUILD_DIR}
+
+docker run --rm -v $(pwd):/cleanbuild -w /cleanbuild lambci/lambda:build-python3.8 \
     python3 -m venv .venv && \
     . ./.venv/bin/activate && \
-    pip install -r requirements-prod.txt -t ${PKG_DIR} && \
-sudo chown -R $UID:$GID ${PKG_DIR}
+    pip install -r lambda_layer/requirements.txt -t ${PKG_DIR} && \
+sudo chown -R $UID:$GID ${BUILD_DIR}
 sudo rm -rf python/botocore
 zip -9yr package.zip python
 sudo rm -rf python
