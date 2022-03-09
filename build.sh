ec03fae1071d9fe84e1e99607c7cfd77cf9dc547
Flabby Ninja <dev@flabbyninja.net>
2022-03-09 21:17:42 +0000

    Updates to build script Fixes #12

--- build.sh
+++ build.sh
@@ -6,17 +6,21 @@
 export BUILD_DIR="build"
 export PKG_DIR="python"
 
-rm -rf ${PKG_DIR} &&\
-    rm package.zip &&\
-    mkdir -p ${PKG_DIR}
+mkdir -p ${BUILD_DIR}
 
-cd ${BUILD_DIR}
+rm -rf ${BUILD_DIR}\/${PKG_DIR} &&\
+    rm ${BUILD_DIR}\/package.zip &&\
+    mkdir -p ${BUILD_DIR}\/${PKG_DIR}
+
 
 docker run --rm -v $(pwd):/cleanbuild -w /cleanbuild lambci/lambda:build-python3.8 \
     python3 -m venv .venv && \
     . ./.venv/bin/activate && \
-    pip install -r lambda_layer/requirements.txt -t ${PKG_DIR} && \
+    pip install -r lambda_layer/requirements.txt -t ${BUILD_DIR}\/${PKG_DIR}
 sudo chown -R $UID:$GID ${BUILD_DIR}
-sudo rm -rf python/botocore
-zip -9yr package.zip python
-sudo rm -rf python
+
+cd ${BUILD_DIR}
+rm -rf ${PKG_DIR}/botocore
+cp -R ../wordlertools ${PKG_DIR}
+zip -9yr package.zip ${PKG_DIR}
+sudo rm -rf ${PKG_DIR}
